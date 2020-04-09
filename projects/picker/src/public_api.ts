df5b042c413af198b0263aece474ea089041b70c
Nat OS <nat.os@pal.net.vn>
2020-04-09 13:19:52 +0700

    Add missing export: OwlMomentDateTimeModule

--- projects/picker/src/public_api.ts
+++ projects/picker/src/public_api.ts
@@ -5,6 +5,7 @@
 export { OwlDateTimeModule } from './lib/date-time/date-time.module';
 export { OwlDateTimeIntl } from './lib/date-time/date-time-picker-intl.service';
 export { OwlNativeDateTimeModule } from './lib/date-time/adapter/native-date-time.module';
+export { OwlMomentDateTimeModule } from './lib/date-time/adapter/moment-adapter/moment-date-time.module';
 export {
   OWL_DATE_TIME_LOCALE_PROVIDER,
   OWL_DATE_TIME_LOCALE,
